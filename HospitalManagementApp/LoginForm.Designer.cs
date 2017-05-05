dcc3d4ac6cb127ee27140d624013b51e0a1672bd
Tien Nguyen <nghuuanhtien@gmail.com>
2017-05-05 17:44:09 +0700

    Design Login Form (beta) - chưa edit viền button, chưa test với db

--- HospitalManagementApp/LoginForm.Designer.cs
+++ HospitalManagementApp/LoginForm.Designer.cs
@@ -28,93 +28,247 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
             this.loginButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.textEditorName = new Infragistics.Win.UltraWinEditors.UltraTextEditor();
-            this.ultraLabel1 = new Infragistics.Win.Misc.UltraLabel();
-            this.ultraLabel2 = new Infragistics.Win.Misc.UltraLabel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
             this.textEditorPassword = new Infragistics.Win.UltraWinEditors.UltraTextEditor();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel2.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel8.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.textEditorName)).BeginInit();
+            this.panel9.SuspendLayout();
+            this.panel10.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.textEditorPassword)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // loginButton
             // 
-            this.loginButton.BackColor = System.Drawing.SystemColors.Window;
-            this.loginButton.Location = new System.Drawing.Point(496, 264);
+            this.loginButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(87)))), ((int)(((byte)(154)))));
+            this.loginButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(74)))), ((int)(((byte)(50)))));
+            this.loginButton.Font = new System.Drawing.Font("Roboto Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.ForeColor = System.Drawing.Color.White;
+            this.loginButton.Location = new System.Drawing.Point(25, 244);
             this.loginButton.Name = "loginButton";
-            this.loginButton.Size = new System.Drawing.Size(114, 38);
+            this.loginButton.Size = new System.Drawing.Size(282, 49);
             this.loginButton.TabIndex = 0;
-            this.loginButton.Text = "Đăng nhập";
+            this.loginButton.Text = "SIGN IN";
             this.loginButton.UseVisualStyleBackColor = false;
             this.loginButton.Click += new System.EventHandler(this.button1_Click);
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(2, 335);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(330, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(2, 335);
+            this.panel3.TabIndex = 6;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(2, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(328, 2);
+            this.panel4.TabIndex = 7;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel5.Location = new System.Drawing.Point(2, 333);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(328, 2);
+            this.panel5.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel6);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(2, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(328, 78);
+            this.panel2.TabIndex = 9;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(87)))), ((int)(((byte)(154)))));
+            this.panel6.ForeColor = System.Drawing.Color.White;
+            this.panel6.Location = new System.Drawing.Point(0, 18);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(15, 60);
+            this.panel6.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto Condensed", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(74)))), ((int)(((byte)(79)))));
+            this.label1.Location = new System.Drawing.Point(28, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SIGN IN";
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.panel8);
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel7.Location = new System.Drawing.Point(2, 80);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(328, 81);
+            this.panel7.TabIndex = 13;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
+            this.panel8.Controls.Add(this.pictureBox1);
+            this.panel8.Controls.Add(this.textEditorName);
+            this.panel8.Location = new System.Drawing.Point(23, 18);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(282, 55);
+            this.panel8.TabIndex = 14;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(236, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(38, 42);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
             // textEditorName
             // 
-            this.textEditorName.DisplayStyle = Infragistics.Win.EmbeddableElementDisplayStyle.Office2013;
-            this.textEditorName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.textEditorName.Location = new System.Drawing.Point(421, 174);
+            this.textEditorName.BorderStyle = Infragistics.Win.UIElementBorderStyle.None;
+            this.textEditorName.Font = new System.Drawing.Font("Roboto Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.textEditorName.Location = new System.Drawing.Point(20, 14);
             this.textEditorName.Name = "textEditorName";
-            this.textEditorName.Size = new System.Drawing.Size(189, 28);
-            this.textEditorName.TabIndex = 1;
-            this.textEditorName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textEditorName_KeyDown);
+            this.textEditorName.Size = new System.Drawing.Size(203, 29);
+            this.textEditorName.TabIndex = 15;
+            this.textEditorName.Text = "username";
             // 
-            // ultraLabel1
+            // panel9
             // 
-            this.ultraLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.ultraLabel1.Location = new System.Drawing.Point(326, 179);
-            this.ultraLabel1.Name = "ultraLabel1";
-            this.ultraLabel1.Size = new System.Drawing.Size(89, 23);
-            this.ultraLabel1.TabIndex = 2;
-            this.ultraLabel1.Text = "Tài khoản";
+            this.panel9.Controls.Add(this.panel10);
+            this.panel9.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel9.Location = new System.Drawing.Point(2, 161);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(328, 77);
+            this.panel9.TabIndex = 14;
             // 
-            // ultraLabel2
+            // panel10
             // 
-            this.ultraLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.ultraLabel2.Location = new System.Drawing.Point(326, 226);
-            this.ultraLabel2.Name = "ultraLabel2";
-            this.ultraLabel2.Size = new System.Drawing.Size(77, 23);
-            this.ultraLabel2.TabIndex = 3;
-            this.ultraLabel2.Text = "Mật khẩu";
+            this.panel10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
+            this.panel10.Controls.Add(this.textEditorPassword);
+            this.panel10.Controls.Add(this.pictureBox2);
+            this.panel10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(150)))), ((int)(((byte)(150)))));
+            this.panel10.Location = new System.Drawing.Point(23, 6);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(282, 55);
+            this.panel10.TabIndex = 14;
             // 
             // textEditorPassword
             // 
-            this.textEditorPassword.DisplayStyle = Infragistics.Win.EmbeddableElementDisplayStyle.Office2013;
-            this.textEditorPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.textEditorPassword.Location = new System.Drawing.Point(421, 221);
+            this.textEditorPassword.BorderStyle = Infragistics.Win.UIElementBorderStyle.None;
+            this.textEditorPassword.Font = new System.Drawing.Font("Roboto Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.textEditorPassword.Location = new System.Drawing.Point(20, 12);
             this.textEditorPassword.Name = "textEditorPassword";
             this.textEditorPassword.PasswordChar = '●';
-            this.textEditorPassword.Size = new System.Drawing.Size(189, 28);
-            this.textEditorPassword.TabIndex = 4;
-            this.textEditorPassword.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textEditorPassword_KeyDown);
+            this.textEditorPassword.Size = new System.Drawing.Size(203, 29);
+            this.textEditorPassword.TabIndex = 17;
+            this.textEditorPassword.Text = "Passcode";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(236, 6);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(38, 42);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 16;
+            this.pictureBox2.TabStop = false;
             // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(984, 461);
-            this.Controls.Add(this.textEditorPassword);
-            this.Controls.Add(this.ultraLabel2);
-            this.Controls.Add(this.ultraLabel1);
-            this.Controls.Add(this.textEditorName);
+            this.ClientSize = new System.Drawing.Size(332, 335);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.loginButton);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
             this.MaximizeBox = false;
             this.Name = "LoginForm";
-            this.Text = "Hopital Management";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel8.ResumeLayout(false);
+            this.panel8.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.textEditorName)).EndInit();
+            this.panel9.ResumeLayout(false);
+            this.panel10.ResumeLayout(false);
+            this.panel10.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.textEditorPassword)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
         private Infragistics.Win.UltraWinEditors.UltraTextEditor textEditorName;
-        private Infragistics.Win.Misc.UltraLabel ultraLabel1;
-        private Infragistics.Win.Misc.UltraLabel ultraLabel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel10;
         private Infragistics.Win.UltraWinEditors.UltraTextEditor textEditorPassword;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        internal System.Windows.Forms.Button loginButton;
     }
 }
