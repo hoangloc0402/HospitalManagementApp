c50c96f9babc50482b9d21de269a63ab6ce7c393
QuocBao <quocbao100@gmail.com>
2017-05-05 21:26:56 +0700

    update ...

--- HospitalManagementApp/Program.cs
+++ HospitalManagementApp/Program.cs
@@ -17,11 +17,11 @@ namespace HospitalManagementApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            string oradb = "DATA SOURCE=127.0.0.1:1521/hospital;PERSIST SECURITY INFO=True;USER ID=MANAGER; PASSWORD= Oracle_1;";
-            OracleConnection conn = new OracleConnection(oradb);  // C
-            conn.Open();
-            Application.Run(new MainForm(conn));
-            //Application.Run(new LoginForm());
+            //string oradb = "DATA SOURCE=127.0.0.1:1521/hospital;PERSIST SECURITY INFO=True;USER ID=MANAGER; PASSWORD= Oracle_1;";
+            //OracleConnection conn = new OracleConnection(oradb);  // C
+            //conn.Open();
+            //Application.Run(new MainForm(conn));
+            Application.Run(new LoginForm());
         }
     }
 }
