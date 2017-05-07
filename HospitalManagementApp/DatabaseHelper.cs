62896f77aa9e515455cf54ec3590526c79d2f9f8
QuocBao <quocbao100@gmail.com>
2017-05-07 14:43:37 +0700

    Revert "Hahaha mừng quá"
    This reverts commit bab22a3ffcb25744fdea2480015fe842f58233c4.

--- HospitalManagementApp/DatabaseHelper.cs
+++ HospitalManagementApp/DatabaseHelper.cs
@@ -32,16 +32,59 @@ namespace HospitalManagementApp
             da.Fill(ds, "A");
             return ds.Tables;
         }
-        public void ThemBN(String mabn, String ten,String diachi,String loai, String manv) {
+        public void ThemBN(String mabn, String ten,String diachi,String loai) {
             OracleCommand cmd = new OracleCommand("THEMBN", conn);
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.Parameters.Add("P_MABN", OracleDbType.Varchar2).Value = mabn;
             cmd.Parameters.Add("P_TENBN", OracleDbType.Varchar2).Value = ten;
             cmd.Parameters.Add("P_DIACHI", OracleDbType.Varchar2).Value = diachi;
             cmd.Parameters.Add("P_LOAI", OracleDbType.Varchar2).Value = loai;
-            cmd.Parameters.Add("P_MANV", OracleDbType.Varchar2).Value = manv;
+            //cmd.Parameters.Add("P_MANV", OracleDbType.Varchar2).Value = manv;
             OracleDataAdapter da = new OracleDataAdapter(cmd);
             cmd.ExecuteNonQuery();
+        }
+        public void ThemSDTBN(String mabn, String sdt)
+        {
+            int a = Int32.Parse(sdt);
+            OracleCommand cmd = new OracleCommand("THEMSDTBN", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("P_MABN", OracleDbType.Varchar2).Value = mabn;
+            cmd.Parameters.Add("P_SDT", OracleDbType.Varchar2).Value = sdt;
+            OracleDataAdapter da = new OracleDataAdapter(cmd);
+            cmd.ExecuteNonQuery();
+        }
+        public DataTableCollection DanhSachThuoc(String maphong, String makhu)
+        {
+            OracleCommand cmd = new OracleCommand("DSTHUOC", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("P_MAPHONG", OracleDbType.Varchar2).Value = maphong;
+            cmd.Parameters.Add("P_MAKHU", OracleDbType.Varchar2).Value = makhu;
+            cmd.Parameters.Add("P_DSTHUOC", OracleDbType.RefCursor).Direction = ParameterDirection.Output;
+            OracleDataAdapter da = new OracleDataAdapter(cmd);
+            cmd.ExecuteNonQuery();
+            DataSet ds = new DataSet();
+            da.Fill(ds, "A");
+            return ds.Tables;
+        }
+        public DataTableCollection TaoBaoCao(String mabn, out String ten,out String tongtien)
+        {
+            OracleCommand cmd = new OracleCommand("TAOBAOCAO", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("P_MABN", OracleDbType.Varchar2).Value = mabn;
+            cmd.Parameters.Add("P_TENBN", OracleDbType.Varchar2, 50).Direction = ParameterDirection.Output;
+            cmd.Parameters.Add("P_BENHAN", OracleDbType.RefCursor).Direction = ParameterDirection.Output;
+            cmd.Parameters.Add("P_CAPHAUTHUAT", OracleDbType.RefCursor).Direction = ParameterDirection.Output;
+            cmd.Parameters.Add("P_DOTDIEUTRI", OracleDbType.RefCursor).Direction = ParameterDirection.Output;
+            cmd.Parameters.Add("P_THUOC", OracleDbType.RefCursor).Direction = ParameterDirection.Output;
+            cmd.Parameters.Add("P_DUNGCU", OracleDbType.RefCursor).Direction = ParameterDirection.Output;
+            cmd.Parameters.Add("P_TONGTIEN", OracleDbType.Int32).Direction = ParameterDirection.Output;
+            OracleDataAdapter da = new OracleDataAdapter(cmd);
+            cmd.ExecuteNonQuery();
+            ten = cmd.Parameters["P_TENBN"].Value.ToString();
+            tongtien = cmd.Parameters["P_TONGTIEN"].Value.ToString();
+            DataSet ds = new DataSet();
+            da.Fill(ds, "A");
+            return ds.Tables;
         }
     }
 }
--- HospitalManagementApp/LoginForm.Designer.cs
+++ HospitalManagementApp/LoginForm.Designer.cs
@@ -29,21 +29,18 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
-            Infragistics.Win.Appearance appearance1 = new Infragistics.Win.Appearance();
-            Infragistics.Win.Appearance appearance2 = new Infragistics.Win.Appearance();
             this.loginButton = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
             this.panel6 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.panel7 = new System.Windows.Forms.Panel();
             this.panel8 = new System.Windows.Forms.Panel();
-            this.textEditorName = new Infragistics.Win.UltraWinEditors.UltraTextEditor();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textEditorName = new Infragistics.Win.UltraWinEditors.UltraTextEditor();
             this.panel9 = new System.Windows.Forms.Panel();
             this.panel10 = new System.Windows.Forms.Panel();
             this.textEditorPassword = new Infragistics.Win.UltraWinEditors.UltraTextEditor();
@@ -51,8 +48,8 @@
             this.panel2.SuspendLayout();
             this.panel7.SuspendLayout();
             this.panel8.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.textEditorName)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEditorName)).BeginInit();
             this.panel9.SuspendLayout();
             this.panel10.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.textEditorPassword)).BeginInit();
@@ -62,17 +59,14 @@
             // loginButton
             // 
             this.loginButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(87)))), ((int)(((byte)(154)))));
-            this.loginButton.FlatAppearance.BorderSize = 0;
-            this.loginButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.loginButton.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.loginButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(74)))), ((int)(((byte)(50)))));
+            this.loginButton.Font = new System.Drawing.Font("Roboto Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.loginButton.ForeColor = System.Drawing.Color.White;
             this.loginButton.Location = new System.Drawing.Point(25, 245);
             this.loginButton.Name = "loginButton";
             this.loginButton.Size = new System.Drawing.Size(282, 49);
             this.loginButton.TabIndex = 0;
-            this.loginButton.Text = "ĐĂNG NHẬP";
-            this.loginButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
-            this.loginButton.UseCompatibleTextRendering = true;
+            this.loginButton.Text = "SIGN IN";
             this.loginButton.UseVisualStyleBackColor = false;
             this.loginButton.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -82,7 +76,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(2, 316);
+            this.panel1.Size = new System.Drawing.Size(2, 335);
             this.panel1.TabIndex = 5;
             // 
             // panel3
@@ -91,7 +85,7 @@
             this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
             this.panel3.Location = new System.Drawing.Point(330, 0);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(2, 316);
+            this.panel3.Size = new System.Drawing.Size(2, 335);
             this.panel3.TabIndex = 6;
             // 
             // panel4
@@ -107,14 +101,13 @@
             // 
             this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.panel5.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel5.Location = new System.Drawing.Point(2, 314);
+            this.panel5.Location = new System.Drawing.Point(2, 333);
             this.panel5.Name = "panel5";
             this.panel5.Size = new System.Drawing.Size(328, 2);
             this.panel5.TabIndex = 8;
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.button1);
             this.panel2.Controls.Add(this.panel6);
             this.panel2.Controls.Add(this.label1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
@@ -122,19 +115,6 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(328, 78);
             this.panel2.TabIndex = 9;
-            // 
-            // button1
-            // 
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(296, 6);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(26, 26);
-            this.button1.TabIndex = 2;
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // panel6
             // 
@@ -148,13 +128,13 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Roboto Cn", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Font = new System.Drawing.Font("Roboto Condensed", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(74)))), ((int)(((byte)(79)))));
             this.label1.Location = new System.Drawing.Point(28, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(187, 42);
+            this.label1.Size = new System.Drawing.Size(137, 46);
             this.label1.TabIndex = 0;
-            this.label1.Text = "ĐĂNG NHẬP";
+            this.label1.Text = "SIGN IN";
             // 
             // panel7
             // 
@@ -168,44 +148,33 @@
             // panel8
             // 
             this.panel8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
-            this.panel8.Controls.Add(this.textEditorName);
             this.panel8.Controls.Add(this.pictureBox1);
+            this.panel8.Controls.Add(this.textEditorName);
             this.panel8.Location = new System.Drawing.Point(23, 18);
             this.panel8.Name = "panel8";
             this.panel8.Size = new System.Drawing.Size(282, 55);
             this.panel8.TabIndex = 14;
             // 
-            // textEditorName
-            // 
-            this.textEditorName.AlphaBlendMode = Infragistics.Win.AlphaBlendMode.Disabled;
-            appearance1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
-            appearance1.BorderAlpha = Infragistics.Win.Alpha.UseAlphaLevel;
-            appearance1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
-            appearance1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(150)))), ((int)(((byte)(150)))));
-            this.textEditorName.Appearance = appearance1;
-            this.textEditorName.AutoSize = false;
-            this.textEditorName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
-            this.textEditorName.Font = new System.Drawing.Font("Roboto", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.textEditorName.Location = new System.Drawing.Point(20, 13);
-            this.textEditorName.Name = "textEditorName";
-            this.textEditorName.Size = new System.Drawing.Size(200, 27);
-            this.textEditorName.TabIndex = 17;
-            this.textEditorName.Text = "Tên đăng nhập";
-            this.textEditorName.UseFlatMode = Infragistics.Win.DefaultableBoolean.True;
-            this.textEditorName.UseOsThemes = Infragistics.Win.DefaultableBoolean.False;
-            this.textEditorName.AfterEnterEditMode += new System.EventHandler(this.textEditorName_AfterEnterEditMode);
-            this.textEditorName.AfterExitEditMode += new System.EventHandler(this.textEditorName_AfterExitEditMode);
-            // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(239, 13);
+            this.pictureBox1.Location = new System.Drawing.Point(236, 6);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox1.Size = new System.Drawing.Size(38, 42);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 16;
             this.pictureBox1.TabStop = false;
+            // 
+            // textEditorName
+            // 
+            this.textEditorName.BorderStyle = Infragistics.Win.UIElementBorderStyle.None;
+            this.textEditorName.Font = new System.Drawing.Font("Roboto Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.textEditorName.Location = new System.Drawing.Point(20, 14);
+            this.textEditorName.Name = "textEditorName";
+            this.textEditorName.Size = new System.Drawing.Size(203, 29);
+            this.textEditorName.TabIndex = 15;
+            this.textEditorName.Text = "username";
             // 
             // panel9
             // 
@@ -229,32 +198,22 @@
             // 
             // textEditorPassword
             // 
-            appearance2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
-            appearance2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
-            appearance2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(150)))), ((int)(((byte)(150)))));
-            this.textEditorPassword.Appearance = appearance2;
-            this.textEditorPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
             this.textEditorPassword.BorderStyle = Infragistics.Win.UIElementBorderStyle.None;
-            this.textEditorPassword.Font = new System.Drawing.Font("Roboto", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.textEditorPassword.Font = new System.Drawing.Font("Roboto Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.textEditorPassword.Location = new System.Drawing.Point(20, 12);
             this.textEditorPassword.Name = "textEditorPassword";
             this.textEditorPassword.PasswordChar = '●';
-            this.textEditorPassword.Size = new System.Drawing.Size(200, 29);
+            this.textEditorPassword.Size = new System.Drawing.Size(203, 29);
             this.textEditorPassword.TabIndex = 17;
-            this.textEditorPassword.Text = "Mật khẩu";
-            this.textEditorPassword.UseFlatMode = Infragistics.Win.DefaultableBoolean.True;
-            this.textEditorPassword.UseOsThemes = Infragistics.Win.DefaultableBoolean.False;
-            this.textEditorPassword.AfterEnterEditMode += new System.EventHandler(this.textEditorPassword_AfterEnterEditMode);
-            this.textEditorPassword.AfterExitEditMode += new System.EventHandler(this.textEditorPassword_AfterExitEditMode);
-            this.textEditorPassword.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textEditorPassword_KeyDown);
+            this.textEditorPassword.Text = "Passcode";
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
-            this.pictureBox2.Location = new System.Drawing.Point(239, 12);
+            this.pictureBox2.Location = new System.Drawing.Point(236, 6);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox2.Size = new System.Drawing.Size(38, 42);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 16;
             this.pictureBox2.TabStop = false;
@@ -264,7 +223,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(332, 316);
+            this.ClientSize = new System.Drawing.Size(332, 335);
             this.Controls.Add(this.panel9);
             this.Controls.Add(this.panel7);
             this.Controls.Add(this.panel2);
@@ -282,8 +241,9 @@
             this.panel2.PerformLayout();
             this.panel7.ResumeLayout(false);
             this.panel8.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.textEditorName)).EndInit();
+            this.panel8.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEditorName)).EndInit();
             this.panel9.ResumeLayout(false);
             this.panel10.ResumeLayout(false);
             this.panel10.PerformLayout();
@@ -300,16 +260,15 @@
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel6;
         private System.Windows.Forms.Panel panel7;
         private System.Windows.Forms.Panel panel8;
+        private Infragistics.Win.UltraWinEditors.UltraTextEditor textEditorName;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panel9;
         private System.Windows.Forms.Panel panel10;
         private Infragistics.Win.UltraWinEditors.UltraTextEditor textEditorPassword;
         private System.Windows.Forms.PictureBox pictureBox2;
         internal System.Windows.Forms.Button loginButton;
-        private System.Windows.Forms.Button button1;
-        private Infragistics.Win.UltraWinEditors.UltraTextEditor textEditorName;
-        private System.Windows.Forms.Panel panel6;
     }
 }
